771cd1bbe434460455058c83b04900bc372dbf0a
Corey Lutrick <coreylutrick@gmail.com>
2018-08-25 11:02:17 -0700

    Add project files.

---
+++ StringNumber Manipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringNumber_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
